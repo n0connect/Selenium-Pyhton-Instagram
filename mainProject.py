d97bf24e0338740c583a6f257ffa5173c64ca271
n0connect <126422643+n0connect@users.noreply.github.com>
2023-11-28 23:01:36 +0300

    mainProject.py

--- mainProject.py
+++ mainProject.py
@@ -2,8 +2,7 @@ import browser
 import foundGender
 import user_account
 
-foundGender.user_file_path()
 user_account.User()
 instagram_link: str = r"https://www.instagram.com/"
 browser.Browser(instagram_link)
-
+foundGender.user_file_path()
